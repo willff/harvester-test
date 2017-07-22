69cab7828196adaff294cfced250b0136922a137
William Ferguson <wffergusonjr@gmail.com>
2017-07-22 13:18:04 -0400

    test

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const alfy = require('alfy');
 
 alfy.fetch('http://matrix.topdish.us/categories/all').then(data => {
+	console.log('data: ', data); // wf
+	console.log('data: ', data.categories); // wf
 	const items = alfy
 		.inputMatches(data.categories, 'title')
 		.map(x => ({
