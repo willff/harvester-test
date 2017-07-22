b460e8f97b4d4b28335030c31048db96f9be3f9a
William Ferguson <wffergusonjr@gmail.com>
2017-07-22 13:29:52 -0400

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const alfy = require('alfy');
 
 alfy.fetch('http://matrix.topdish.us/categories/all').then(data => {
 	const items = alfy
-		.inputMatches(data.categories, 'title')
+		.inputMatches(data.categories, 'category')
 		.map((x, index) => ({
 			title: x.category,
 			subtitle: x.category,
