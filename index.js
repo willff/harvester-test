a0d4d700956701cbe4d7b0c13ab1079a16f9924b
Will Ferguson <wffergusonjr@gmail.com>
2017-11-22 18:46:57 -0500

    add parents as subtitle;

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 'use strict';
 const alfy = require('alfy');
 
+const getParents = (parents) => {
+	const list = parents.map(x => {
+		return x.rankingName;
+	});
+	return `parents: ${list.join(', ')}`;
+};
+
 alfy.fetch('http://matrix.topdish.us/categories/all').then(data => {
 	const cats = data.categories;
 	const items = alfy
 		.inputMatches(cats, 'category')
 		.map(x => ({
 			title: x.category,
-			subtitle: x.category,
+			subtitle: getParents(x.parents),
 			arg: `cat: ${x.category}`,
 		}));
 
